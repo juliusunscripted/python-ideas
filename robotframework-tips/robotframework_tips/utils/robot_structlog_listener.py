5b5a195543796789c3c9177e34483d7d507cd59c
suiluj <suiluj@users.noreply.github.com>
2025-06-19 18:49:02 +0200

    improved logging

--- robotframework-tips/robotframework_tips/utils/robot_structlog_listener.py
+++ robotframework-tips/robotframework_tips/utils/robot_structlog_listener.py
@@ -1,5 +1,7 @@
 import structlog
 from robotframework_tips.utils.structlog_utils import configure_structlog
+import logging
+import sys
 
 log = structlog.stdlib.get_logger()
 
@@ -11,7 +13,11 @@ class StructlogListener:
 
     def __init__(self):
         log.info("initializing structlog...")
+        # logging.basicConfig(
+        #     format="%(message)s", stream=sys.stderr, level=logging.INFO, force=True
+        # )
         configure_structlog()
+
         log.info("structlog is initialized now")
 
     # message (structlog as json)
--- robotframework-tips/robotframework_tips/utils/structlog_utils.py
+++ robotframework-tips/robotframework_tips/utils/structlog_utils.py
@@ -1,8 +1,49 @@
 # https://github.com/hynek/structlog/issues/546#issuecomment-1741258252
 import logging
 import structlog
+from pprint import pprint
+from robot.api.logger import debug, info, warn, error
+import sys
 
 from structlog.typing import EventDict, WrappedLogger
+
+
+class RobotLog:
+    def __init__(self):
+        pass
+
+    def __call__(
+        self, logger: WrappedLogger, name: str, event_dict: EventDict
+    ) -> EventDict:
+
+        copied_event_dict = dict(event_dict)
+        copied_event_dict.pop("filename", None)
+        copied_event_dict.pop("lineno", None)
+        copied_event_dict.pop("timestamp", None)
+        level = copied_event_dict.pop("level", None)
+        event = copied_event_dict.pop("event", None)
+
+        logger_func = debug
+
+        match level:
+            case "debug":
+                logger_func = debug
+            case "info":
+                logger_func = info
+            case "warning":
+                logger_func = warn
+            case "error":
+                logger_func = error
+            case _:
+                print("unexpected log level", level)
+                logger_func = debug
+
+        msg = f"{event}"
+        if copied_event_dict:
+            msg += f" | {copied_event_dict}"
+        logger_func(msg=msg, html=False)
+
+        return event_dict
 
 
 class LogJump:
@@ -24,28 +65,33 @@ class LogJump:
         return event_dict
 
 
-def configure_structlog(colors: bool = True):
+def configure_structlog(colors: bool = True, robotframework_log: bool = True):
+    processors = [
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.add_log_level,
+        structlog.processors.StackInfoRenderer(),
+        structlog.dev.set_exc_info,
+        structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
+        structlog.processors.CallsiteParameterAdder(
+            [
+                # add either pathname or filename and then set full_path to True or False in LogJump below
+                # structlog.processors.CallsiteParameter.PATHNAME,
+                structlog.processors.CallsiteParameter.FILENAME,
+                structlog.processors.CallsiteParameter.LINENO,
+            ],
+        ),
+    ]
+    if robotframework_log:
+        processors.append(RobotLog())
+    processors.append(LogJump(full_path=False))
+
+    # if not robotframework_log:
+    processors.append(structlog.dev.ConsoleRenderer(colors=colors))
     structlog.configure(
-        processors=[
-            structlog.contextvars.merge_contextvars,
-            structlog.processors.add_log_level,
-            structlog.processors.StackInfoRenderer(),
-            structlog.dev.set_exc_info,
-            structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S", utc=False),
-            structlog.processors.CallsiteParameterAdder(
-                [
-                    # add either pathname or filename and then set full_path to True or False in LogJump below
-                    # structlog.processors.CallsiteParameter.PATHNAME,
-                    structlog.processors.CallsiteParameter.FILENAME,
-                    structlog.processors.CallsiteParameter.LINENO,
-                ],
-            ),
-            LogJump(full_path=False),
-            structlog.dev.ConsoleRenderer(colors=colors),
-        ],
+        processors=processors,
         wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
         context_class=dict,
-        logger_factory=structlog.PrintLoggerFactory(),
+        logger_factory=structlog.PrintLoggerFactory(file=sys.stderr),
         cache_logger_on_first_use=False,
     )
 
