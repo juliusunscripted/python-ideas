c32129f6144af0c3f0fcbb17b60d1c2dc7fa70f2
suiluj <suiluj@users.noreply.github.com>
2025-06-22 17:27:22 +0200

    add comment to robotframework_mode

--- robotframework-tips/robotframework_tips/examples/script_without_robot_logger.py
+++ robotframework-tips/robotframework_tips/examples/script_without_robot_logger.py
@@ -4,7 +4,12 @@ import structlog
 
 # %%
 
+
+# this will work even if robotframework is not installed
+# (but fail with explanation in case robotframework_mode gets set to True and robotframework is not installed)
 configure_structlog(robotframework_mode=False, full_path=False)
+
+# %%
 
 log = structlog.stdlib.get_logger()
 
